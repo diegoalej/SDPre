db4000594ead0eac4ec40dcef6a685cd49eddc79
Diego Hoyos <hoyosdiegoa@gmail.com>
2019-12-28 16:14:19 -0700

    finish jthw ex 2

---
+++ JavaTheHardWay/GasolineReceipt.java
@@ -0,0 +1,16 @@
+public class GasolineReceipt {
+  public static void main(String[] args){
+    System.out.println("+---------------------------+");
+    System.out.println("|                           |");
+    System.out.println("|        CORNER STORE       |");
+    System.out.println("|                           |");
+    System.out.println("|  2019-12-28       4:00PM  |");
+    System.out.println("|                           |");
+    System.out.println("|  GALLONS:           10.9  |");
+    System.out.println("|  PRICE PER GALLON: $2.80  |");
+    System.out.println("|                           |");
+    System.out.println("|  TOTAL TODAY:     $30.52  |");
+    System.out.println("|                           |");
+    System.out.println("+---------------------------+");
+  }
+}
