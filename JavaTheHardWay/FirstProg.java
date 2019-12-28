83861d922c8ea5c5a6761dd9a8232b667da0e1a2
Diego Hoyos <hoyosdiegoa@gmail.com>
2019-12-28 15:54:12 -0700

    finish jthw ex 1

---
+++ JavaTheHardWay/FirstProg.java
@@ -0,0 +1,6 @@
+public class FirstProg {
+    public static void main(String[] args){
+        System.out.println("I, Diego Hoyos, am determined to learn how to code.");
+        System.out.println("Today's date is Saturday, December 28, 2019.");
+  }
+}
