b8dba5b9dc283e5efc3a8d74938572ed7619eaf8
Diego Hoyos <hoyosdiegoa@gmail.com>
2019-12-28 17:31:30 -0700

    complete objective1lab3

---
+++ Labs/Objective1Lab3.java
@@ -0,0 +1,7 @@
+public class Objective1Lab3 {
+    public static void main(String[] args) {
+      System.out.println("Name: Diego Hoyos");
+      System.out.println("Age: 31");
+      System.out.println("State: Loja, Ecuador");
+    }
+}
