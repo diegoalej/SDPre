72ae0860519bd5e1c3d8283ba8f4799ec420e71c
Diego Hoyos <hoyosdiegoa@gmail.com>
2019-12-30 11:10:54 -0700

    finish jthw ex 19

---
+++ JavaTheHardWay/BMICategories.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+public class BMICategories {
+    public static void main (String[] args){
+        Scanner keyboard = new Scanner(System.in);
+        double bmi, m, kg, lb, ft, in;
+
+        System.out.print( "Your height (feet only): ");
+        ft = keyboard.nextDouble();
+
+        System.out.print( "Your height (inches): ");
+        in = keyboard.nextDouble();
+        //convert ft to inches first, then to cm, then meters
+        m = (((ft*12)+in) * 2.54)/100;
+
+        System.out.print("Your weight in pounds: ");
+        lb = keyboard.nextDouble();
+        //convert kg to lb
+        kg = lb*0.453592;
+
+        bmi = kg / (m*m);
+
+        // System.out.print("Enter your BMI: ");
+        // bmi = keyboard.nextDouble();
+
+        System.out.print("BMI category: ");
+        if(bmi < 15.0) {
+            System.out.println("very severely underweight");
+        }
+        else if(bmi <= 16.0) {
+            System.out.println("severely underweight");
+        }
+        else if(bmi < 18.5) {
+            System.out.println("underweight");
+        }
+        else if(bmi < 25.0) {
+            System.out.println("normal weight");
+        }
+        else if(bmi < 30.0) {
+            System.out.println("overweight");
+        }
+        else if(bmi < 35.0) {
+            System.out.println("moderately obese");
+        }
+        else if(bmi < 40.0) {
+            System.out.println("severely obese");
+        }
+        else  {
+            System.out.println("very severely/\"morbidly\" obese");
+        }
+    }
+}
