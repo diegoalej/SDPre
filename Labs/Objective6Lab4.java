c03394dcc1fe362c51cab4d390ec3badb0073ff8
Diego Hoyos <hoyosdiegoa@gmail.com>
2020-01-05 16:41:04 -0700

    complete objective6lab4

---
+++ Labs/Objective6Lab4.java
@@ -0,0 +1,13 @@
+public class Objective6Lab4 {
+      public static void main(String[] args) {
+          int sum = 0;
+          int counter = 1;
+
+          while(counter <= 20){
+            sum += counter;
+            counter++;
+          }
+          System.out.println(sum);
+
+      }
+}
