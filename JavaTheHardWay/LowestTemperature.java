9180cf11f452d30b63119b7c4d3ae7c3d90fac20
Diego Hoyos <hoyosdiegoa@gmail.com>
2020-01-15 10:59:11 -0700

    finish jthw ex 52

---
+++ JavaTheHardWay/LowestTemperature.java
@@ -0,0 +1,44 @@
+import java.net.URL;
+import java.util.Scanner;
+
+public class LowestTemperature {
+      public static void main(String[] args) throws Exception {
+            String urlbase = "https://learnjavathehardway.org/txt/";
+            double[] temps = arrayFromUrl(urlbase + "avg-daily-temps-atx.txt");
+
+            System.out.println(temps.length + " temperatures in database.");
+
+            double lowest = 9999.99;
+            double highest = -1.99;
+
+
+            for (int i = 0; i < temps.length ; i++ ) {
+                  if (temps[i] < lowest) {
+                        lowest = temps[i];
+                  }
+                 if (temps[i] > highest) {
+                        highest = temps [i];
+                  }
+            }
+
+            System.out.println("The lowest average daily temperature was " + lowest + "F (" + fToC(lowest) + "C)");
+            System.out.println("The highest average daily temperature was " + highest + "F (" + fToC(highest) + "C)");
+      }
+
+      public static double[] arrayFromUrl(String url) throws Exception {
+            Scanner fin = new Scanner((new URL(url)).openStream());
+            int count = fin.nextInt();
+
+            double[] dubs = new double[count];
+
+            for (int i = 0; i < dubs.length ; i++ ) {
+                  dubs[i] = fin.nextDouble();
+            }
+            fin.close();
+            return dubs;
+      }
+
+      public static double fToC(double f) {
+            return (f-32)*5/9;
+      }
+}
